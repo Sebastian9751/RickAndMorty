4a500cfaf954c4e0d54551997836f350020ce227
Sebas <sebastiansilvan97@gmail.com>
2023-03-04 07:53:28 -0500

    Fragment detail completado, con paso de parametros seteados

--- app/src/main/java/com/example/rickandmorty/ui/home/detail/CharacterDetailFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/home/detail/CharacterDetailFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.rickandmorty.R
@@ -14,24 +16,23 @@ class CharacterDetailFragment : Fragment() {
     private var _binding: FragmentCharacterDetailBinding? = null
     private val binding get() = _binding!!
     private val args: CharacterDetailFragmentArgs by navArgs()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_detail, container, false)
-
+        _binding = FragmentCharacterDetailBinding.inflate(inflater,container,false)
+        return  binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textName.text = "hi"
+
+        binding.textName.text =  "${args.resultModel.name}"
+
     }
     override fun onDestroyView() {
         super.onDestroyView()
