4b7ee1832fbf037945dbaa163627cea021ca853f
Sebas <sebastiansilvan97@gmail.com>
2023-03-08 03:20:26 -0500

    Implementacion de un Grid para Character Alive

--- app/src/main/java/com/example/rickandmorty/ui/characterAlive/CharacterAliveFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/characterAlive/CharacterAliveFragment.kt
@@ -5,11 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rickandmorty.data.models.ResultsModel
 import com.example.rickandmorty.databinding.FragmentCharacterAliveBinding
@@ -37,7 +37,7 @@ class CharacterAliveFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.swipe.isEnabled = false
         binding.swipe.isRefreshing = true
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.recyclerView.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
         setRecyclerView()
     }
