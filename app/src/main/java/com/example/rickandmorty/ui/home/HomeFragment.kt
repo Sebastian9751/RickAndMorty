2ab8c5deba64628a5bd92707a27db44496981d01
Sebas <sebastiansilvan97@gmail.com>
2023-03-08 13:56:18 -0500

    Formateo de codigo

--- app/src/main/java/com/example/rickandmorty/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/home/HomeFragment.kt
@@ -21,7 +21,10 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val result = GetCharacterUseCase()
-    private var PAGUE =1
+    private val characterList = mutableListOf<ResultsModel>()
+    private var currentVisiblePosition =
+        0 // variable para guardar la posición actual del reciclador
+    private var PAGUE = 1
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,7 +37,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.swipe.isEnabled = false
-
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
         setRecyclerView()
@@ -45,17 +47,12 @@ class HomeFragment : Fragment() {
                 val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
                 val totalItemCount = layoutManager.itemCount
                 if (lastVisibleItemPosition == totalItemCount - 1) {
-                    PAGUE+=1
+                    PAGUE += 1
                     setRecyclerView()
                 }
             }
         })
-
     }
-
-    private val characterList = mutableListOf<ResultsModel>()
-
-    private var currentVisiblePosition = 0 // variable para guardar la posición actual del reciclador
 
     private fun setRecyclerView() {
         lifecycleScope.launch {
@@ -67,7 +64,6 @@ class HomeFragment : Fragment() {
                 characterList.addAll(results)
                 val adapter = HomeAdapter(characterList) { ch -> onItemSelect(ch) }
                 binding.recyclerView.adapter = adapter
-
                 // restaurar la posición del reciclador después de actualizar el conjunto de datos
                 binding.recyclerView.scrollToPosition(currentVisiblePosition)
             }
@@ -75,10 +71,7 @@ class HomeFragment : Fragment() {
             Log.i("hellooRk", "$response")
         }
     }
-
-
-
-
+    
 
     private fun onItemSelect(characterss: ResultsModel) {
         val direction =
